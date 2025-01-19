0d8ccb5ed0db613322f6d2e3fe2e374ca9633e9f
Vitaly <vitaliy.zayakin@gmail.com>
2025-01-19 17:11:40 +0500

    docs: add description to work with ESM

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,15 +1,24 @@
+/*
+  This solution uses ES modules. Add the line “type”: “module”,
+    to package.json immediately after the line “description”: “html-builder task for RSSchool”.
+  For eslint and prettier: rename .eslintrc.js to .eslintrc.cjs, .prettierrc.js to .prettierrc.cjs,
+    add sourceType: 'module' to parserOptions of .eslintrc.cjs (parserOptions: { ecmaVersion: 13, sourceType: 'module' }).
+*/
+
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
 import { createReadStream } from 'node:fs';
 import { pipeline } from 'node:stream/promises';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const filePath = resolve(__dirname, 'text.txt');
 
-const readStream = createReadStream(filePath, { encoding: 'utf-8' });
+const FILE_PATH = resolve(__dirname, 'text.txt');
+const ERROR_MESSAGE = 'Oops! Something went wrong';
 
 try {
+  const readStream = createReadStream(FILE_PATH, { encoding: 'utf-8' });
+
   await pipeline(readStream, process.stdout);
 } catch (error) {
-  console.log(error instanceof Error ? error.message : 'Something went wrong');
+  console.log(error instanceof Error ? error.message : ERROR_MESSAGE);
 }
